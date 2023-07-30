8e249c56dfbf9dda66d959650239c4d0df6586f1
Karolis <karolisdarbinis2@gmail.com>
2023-07-30 21:52:25 +0300

    Info update

--- src/Components/Monthly/Monthly.jsx
+++ src/Components/Monthly/Monthly.jsx
@@ -1,8 +1,44 @@
+import { FaDollarSign } from "react-icons/fa";
+
 function Monthly() {
   return (
-    <div className="news">
-      <h1>sup</h1>
-    </div>
+    <>
+      <div className="top">
+        <div className="news">
+          <div className="cont">
+            <div className="img">
+              <div className="burbulas">
+                <FaDollarSign style={{ color: "white", fontSize: "1.2em" }} />
+              </div>
+              <span className="name" style={{ color: "grey", fontWeight: 600 }}>
+                Monthly Recurring Revenue
+              </span>
+            </div>
+            <p>Income</p>
+            <h1>
+              <FaDollarSign style={{ color: "skyblue", fontSize: "0.6em" }} />
+              156,098.1
+            </h1>
+            <span>
+              <span style={{ color: "limegreen" }}>2.9%</span> vs $300.3k last
+              year
+            </span>
+          </div>
+        </div>
+        <div className="cont2">
+          <div className="img"></div>
+          <p>Spend</p>
+          <h1>
+            <FaDollarSign style={{ color: "skyblue", fontSize: "0.6em" }} />
+            80,112.02
+          </h1>
+          <span>
+            <span style={{ color: "limegreen" }}>2.9%</span> vs $300.3k last
+            year
+          </span>
+        </div>
+      </div>
+    </>
   );
 }
 export default Monthly;
