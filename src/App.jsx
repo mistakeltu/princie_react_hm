d452555933ebe54519503e92bb156513da40a69c
Karolis <karolisdarbinis2@gmail.com>
2023-07-30 11:04:35 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+import Top from "./Components/Top.jsx";
+
+function App() {
+  return <Top />;
+}
+
+export default App;
---
+++ src/Components/Top.jsx
@@ -0,0 +1,11 @@
+import "./top.css";
+
+function Top() {
+  return (
+    <div>
+      <h1 className="bandymas">Waaaazeee</h1>
+    </div>
+  );
+}
+
+export default Top;
